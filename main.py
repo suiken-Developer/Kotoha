ea6d4da8a0957f69c2d6d3b351ee9f086f59641d
Sui8 <66006042+Sui8@users.noreply.github.com>
2024-08-05 02:35:53 +0900

    v4.17.3
    /url, /gosenが正常に動作しない問題を解決

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ OWNER = int(os.getenv("OWNER"))
 STARTUP_LOG = int(os.getenv("STARTUP_LOG"))
 ERROR_LOG = int(os.getenv("ERROR_LOG"))
 PREFIX = "k."  # Default Prefix
-VERSION = "4.17.2"
+VERSION = "4.17.3"
 
 # Gemini
 AIMODEL_NAME = "gemini-1.5-pro-latest"
@@ -843,7 +843,7 @@ async def url(ctx: discord.Interaction, url: str):
     await ctx.response.defer()
 
     request = requests.post(
-        "https://ur7.cc/yourls-api.php?username={UR7_USERNAME}&password={UR7_PASSWORD}&action=shorturl&format=json&url={url}"
+        f"https://ur7.cc/yourls-api.php?username={UR7_USERNAME}&password={UR7_PASSWORD}&action=shorturl&format=json&url={url}"
         )
 
     r = request.json()
@@ -1145,7 +1145,7 @@ class GosenChoen(discord.ui.Modal, title='「5000兆円欲しい！」ジェネ�
     )
 
     async def on_submit(self, ctx: discord.Interaction):
-        url = "https://gsapi.cbrx.io/image?top={self.line1.value}&bottom={self.line2.value}&type=png"
+        url = f"https://gsapi.cbrx.io/image?top={self.line1.value}&bottom={self.line2.value}&type=png"
 
         try:
             embed = discord.Embed()
